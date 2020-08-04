0002a48dbae29317e9a9d0980c0e76a52bcc3068
kowalkowska-just <kowalkowska.just@gmail.com>
2020-08-04 23:53:48 +0200

    Initial Commit

---
+++ InspoQuotes/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  InspoQuotes
+//
+//  Created by Justyna Kowalkowska on 04/08/2020.
+//  Copyright © 2020 Justyna Kowalkowska. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
